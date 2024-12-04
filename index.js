48a0eea49a94254089bfe9a8cea0e56e792a06c8
JMahajan1 <mahajanjatinit@gmail.com>
2024-12-05 00:42:44 +0530

    Add login function

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ function signup(userName) {
 }
 console.log(signup("user1")); 
 console.log(signup("newUser"));
+
+function login(userName, password) {
+    const users = ["user1", "user2", "user3"]; // Predefined usernames
+    const correctPassword = "Emp@123";
+
+    if (!users.includes(userName)) {
+        return "User Not Found, Please Signup";
+    } else if (password !== correctPassword) {
+        return "Wrong Password....";
+    } else {
+        return "Login Successful...";
+    }
+}
+
+console.log(login("user1", "Emp@123")); 
+console.log(login("user4", "Emp@123"));
+console.log(login("user1", "wrongPassword"));
